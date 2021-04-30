fbb734caf62f22cc611ca00aafb44f140ac769e3
Keishi.Y <ky0324880@gmail.com>
2021-04-30 17:08:03 +0900

    add apply log

--- src/pages/AppInfo.js
+++ src/pages/AppInfo.js
@@ -12,10 +12,26 @@ const AppInfo = () => {
         <h2 className="text-center card-header">更新履歴</h2>
         <ul className="list-group">
           <li className="list-group-item">
-            2021/04/16　ログイン機能/アカウント情報画面/アプリ情報画面を実装しました。
+            <p>2021/04/30</p>
+            <p>リストの並び替え機能を実装しました。</p>
+            <p>リストの絞り込み機能を実装しました。</p>
           </li>
           <li className="list-group-item">
-            2021/04/15　アプリを作成しました。
+            <p>2021/04/23</p>
+            <p>
+              フレーム情報の表示（複数テーブルへのアクセス）ができるようになりました。
+            </p>
+            <p>ログイン前後でホームページの表示内容を変更しました。</p>
+          </li>
+          <li className="list-group-item">
+            <p>2021/04/16</p>
+            <p>
+              ログイン機能/アカウント情報画面/アプリ情報画面を実装しました。
+            </p>
+          </li>
+          <li className="list-group-item">
+            <p>2021/04/15</p>
+            <p>アプリを作成しました。</p>
           </li>
         </ul>
       </div>
